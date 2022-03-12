d4d7ba363e781162cbfb3f20152ce2f447e6f9b8
hugosenari <hugosenari@gmail.com>
2022-03-12 03:35:10 -0300

    chore: re index tic80

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''TIC-80 bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tic80-master.flake = false;
+  inputs.src-tic80-master.owner = "thisago";
+  inputs.src-tic80-master.ref   = "refs/heads/master";
+  inputs.src-tic80-master.repo  = "tic80";
+  inputs.src-tic80-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tic80-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tic80-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}

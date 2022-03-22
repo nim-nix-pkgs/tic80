924c4315405cd0f5216383971b7aaa5aa597b05b
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:27 -0300

    chore: re-index ../nimpkgs/t/tic80

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-tic80-master.flake = false;
   inputs.src-tic80-master.owner = "thisago";
-  inputs.src-tic80-master.ref   = "refs/heads/master";
+  inputs.src-tic80-master.ref   = "master";
   inputs.src-tic80-master.repo  = "tic80";
   inputs.src-tic80-master.type  = "github";
   
